712702fb6d3d547f911750275ce63d6b70ceb567
devanhurst <devan.hurst@gmail.com>
2015-11-18 10:24:43 -0500

    Added seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#User ADMIN owns restaurant EL CABALLITO has two TABLES sharing one RESERVATION
+#User ADMIN owns restaurant EL CABALLITO has two TABLES sharing one RESERVATION.
 User.create(first_name:"Admin", last_name:"Istrator", email:"admin@app.com", password:"admin")
 Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca", user_id:1)
 Table.create(capacity:8, timeslot:Time.now, booked:false, restaurant_id:1)
