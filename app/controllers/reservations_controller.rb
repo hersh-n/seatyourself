65fc08b80247ec3b1177a81569c138a824aefa67
Hershel Nashman <hershel.nashman@gmail.com>
2015-11-18 16:32:43 -0500

    Reservation Controller created

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,51 @@
+class ReservationsController < ApplicationController
+
+	def index
+		@reservations = current_user.reservations
+	end
+
+	def show
+		@reservation = Reservation.find(params[:id])
+	end
+
+	def new
+		@reservations = Reservation.new
+	end
+
+	def create
+		@reservations = Reservation.new(reservation_params)
+		@reservations.user  = current_user
+		if @reservations.save
+			redirect_to reservations_path
+		else
+			render :new
+ 	end
+
+	end
+
+	def destroy
+		@reservation = Reservation.find(params[:id])
+		@reservation.destroy
+		redirect_to reservations_path
+
+	end
+
+	def edit
+		@reservation = Reservation.find(params[:id])
+	end
+
+	def update
+		@reservation = Reservation.find(params[:id])
+
+		if @reservation.update_attributes(contact_params)
+			redirect_to action: :show
+		else
+			render :edit
+		end
+	end
+
+	private
+	def reservation_params
+		params.require(:reservation).permit(:party_size)
+	end
+end
