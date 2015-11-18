9960314db5399efbf617001fe26656fa5abbab1b
devanhurst <devan.hurst@gmail.com>
2015-11-18 10:32:47 -0500

    Seeds.rb fixed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#User ADMIN owns restaurant EL CABALLITO has two TABLES sharing one RESERVATION.
+#No relationships between these objects have been set.
 User.create(first_name:"Admin", last_name:"Istrator", email:"admin@app.com", password:"admin")
-Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca", user_id:1)
-Table.create(capacity:8, timeslot:Time.now, booked:false, restaurant_id:1)
-Table.create(capacity:2, timeslot:Time.now, booked:false, restaurant_id:1)
-Reservation.create(party_size:8, user_id:1)
+Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca")
+Table.create(capacity:8, timeslot:Time.now, booked:false)
+Table.create(capacity:2, timeslot:Time.now, booked:false)
+Reservation.create(party_size:8, user:admin)
