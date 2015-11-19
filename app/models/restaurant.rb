6318969d7796cea2795fb8dcb4ab4117947f6126
devanhurst <devan.hurst@gmail.com>
2015-11-19 15:24:27 -0500

    Add Restaurant.find_tables_for_party and Restaurant.book_tables.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
+
 	belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
 	has_many :tables
 	has_many :reservations, through: :tables
@@ -11,5 +12,26 @@ class Restaurant < ActiveRecord::Base
 			capacity += table.capacity
 		end
 		return capacity
+	end
+
+	def find_tables_for_party(party_size, time)
+		available_tables = self.tables.order(capacity: :asc).where(booked: false, timeslot: time)
+		(1..available_tables.length).each do |i|
+			available_tables.combination(i).to_a.each do |combination|
+				combination_capacity = 0
+				combination.each do |table|
+					combination_capacity += table.capacity
+				end
+				if combination_capacity >= party_size
+					return combination
+				end
+			end
+		end
+	end
+
+	def book_tables(table_combination, reservation_id)
+		table_combination.each do |table|
+			table.book(reservation_id)
+		end
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,11 @@
 #No relationships between these objects have been set.
 
 time = DateTime.new(2016, 1, 1, 19)
+
 User.create(first_name:"Admin", last_name:"Istrator", email:"admin@app.com", password:"admin")
-Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca")
-Table.create(capacity:8, timeslot:Time.now, booked:false)
-Table.create(capacity:2, timeslot:Time.now, booked:false)
-Reservation.create(party_size:8)
+tequila = Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca")
+Table.create(capacity:10, timeslot:time, booked:false, restaurant:tequila)
+Table.create(capacity:8, timeslot:time, booked:false, restaurant:tequila)
+Table.create(capacity:6, timeslot:time, booked:false, restaurant:tequila)
+Table.create(capacity:4, timeslot:time, booked:false, restaurant:tequila)
+Table.create(capacity:2, timeslot:time, booked:false, restaurant:tequila)
