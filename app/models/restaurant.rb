795eb0c22779aae218fddbe34461a6b183f919d2
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-19 16:04:13 -0500

    Refactoring code for restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,19 +5,19 @@ class Restaurant < ActiveRecord::Base
 	has_many :reservations, through: :tables
 	has_many :users, through: :reservations
 
+	validates :name, presence: true
+
 	def get_current_capacity(time)
 		capacity = 0
-		available_tables = self.tables.where(booked: false, timeslot: time)
-		available_tables.each do |table|
+		available_tables(time).each do |table|
 			capacity += table.capacity
 		end
 		return capacity
 	end
 
 	def find_tables_for_party(party_size, time)
-		available_tables = self.tables.order(capacity: :asc).where(booked: false, timeslot: time)
-		(1..available_tables.length).each do |i|
-			available_tables.combination(i).to_a.each do |combination|
+		(1..available_tables(time).length).each do |i|
+			available_tables(time).combination(i).to_a.each do |combination|
 				combination_capacity = 0
 				combination.each do |table|
 					combination_capacity += table.capacity
@@ -32,6 +32,12 @@ class Restaurant < ActiveRecord::Base
 	def book_tables(table_combination, reservation_id)
 		table_combination.each do |table|
 			table.book(reservation_id)
+			table.save
 		end
+	end
+
+	private
+	def available_tables(time)
+		self.tables.order(capacity: :asc).where(booked: false, timeslot: time)
 	end
 end
