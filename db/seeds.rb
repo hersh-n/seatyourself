7ae5bcefc0322f99b3d92f7162bf6a0ccade3812
devanhurst <devan.hurst@gmail.com>
2015-11-21 14:46:29 -0500

    Add attributes to Restaurant.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 time = DateTime.new(2016, 1, 1, 19)
 
 User.create(first_name:"Admin", last_name:"Istrator", email:"admin@app.com", password:"admin")
-tequila = Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca")
+tequila = Restaurant.create(name:"El Caballito Tequila Bar", address:"220 King Street W.", summary:"Tacos and Pitchers", website:"www.elcaballito.ca", rating:4.7, cuisine:"Mexican", cost:4)
 Table.create(capacity:10, timeslot:time, booked:false, restaurant:tequila)
 Table.create(capacity:8, timeslot:time, booked:false, restaurant:tequila)
 Table.create(capacity:6, timeslot:time, booked:false, restaurant:tequila)
